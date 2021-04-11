4d6b27ecd2f203eae6b10012d4ea229884c29891
SpaceKatt <spacekattpoispin@gmail.com>
2021-04-11 15:53:21 -0700

    fix minor bug

--- src/output/jsonOutput.ts
+++ src/output/jsonOutput.ts
@@ -3,7 +3,7 @@ import { Output } from '../interfaces';
 export class JsonOutput implements Output {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   print(data: any): void {
-    const outuptMsg = JSON.stringify(2, null, data);
+    const outuptMsg = JSON.stringify(data, null, 2);
     console.log(outuptMsg);
   }
 }
