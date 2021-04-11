39fc5042fb9c1c076553b679bd4514fa41428249
SpaceKatt <spacekattpoispin@gmail.com>
2021-04-11 15:15:10 -0700

    add constants for output types

---
+++ src/constants/ftrucliConstants.ts
@@ -0,0 +1,6 @@
+// intent, split constants into semantic groups
+// everything in one class, for now, due to time constraints
+export class FtrucliConstans {
+  static readonly outputJson: string = "json";
+  static readonly outputTable: string = "table";
+}
