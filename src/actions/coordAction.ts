b2e9bfd366b2d917962bd7e7dd8b89a0d81576a9
SpaceKatt <spacekattpoispin@gmail.com>
2021-04-11 15:53:31 -0700

    add output to coord action

--- src/actions/coordAction.ts
+++ src/actions/coordAction.ts
@@ -1,16 +1,24 @@
-import { CommandLineAction } from "@rushstack/ts-command-line";
+import { JsonOutput } from '../output';
+
+import { CommandLineAction } from '@rushstack/ts-command-line';
 
 export class CoordAction extends CommandLineAction {
   constructor() {
     super({
-      actionName: "coord",
+      actionName: 'coord',
       summary:
-        "Finds food trucks within a specified distance from a coordinate pair.",
-      documentation: "TODO: more docs here.",
+        'Finds food trucks within a specified distance from a coordinate pair.',
+      documentation: 'TODO: more docs here.',
     });
   }
 
-  protected async onExecute(): Promise<void> {}
+  protected async onExecute(): Promise<void> {
+    const data = { msg: 'reeee' };
+
+    const output = new JsonOutput();
+
+    output.print(data);
+  }
 
   protected onDefineParameters(): void {}
 }
