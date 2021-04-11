b27227790783069d74a9275deb53a4946172f5c9
SpaceKatt <spacekattpoispin@gmail.com>
2021-04-11 16:13:46 -0700

    add missing method to interface

--- src/interfaces/queryBuilder.ts
+++ src/interfaces/queryBuilder.ts
@@ -1,5 +1,6 @@
 export interface QueryBuilder {
   select(field: string): QueryBuilder;
+  withDistance(distance: number): QueryBuilder;
   withLat(lat: string): QueryBuilder;
   withLong(long: string): QueryBuilder;
   withLimit(limit: number): QueryBuilder;
