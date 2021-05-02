11c30729adcdbc997fd1460d402f495e9530cefd
SpaceKatt <spacekattpoispin@gmail.com>
2021-05-02 15:07:04 -0700

    Add output factory

---
+++ src/factories/outputFactory.ts
@@ -0,0 +1,13 @@
+import { OutputType, Output } from '../interfaces';
+import { JsonOutput } from '../output';
+
+export class OutputFactory {
+  static createOutput(kind: OutputType): Output {
+    switch (kind) {
+      case OutputType.prettyJson:
+        return new JsonOutput();
+      default:
+        throw new TypeError(`Unknown type ${kind} given to OutputFactory`);
+    }
+  }
+}
