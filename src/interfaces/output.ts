4bf6d8339c040a61fb2d6d9e7412d799a15fa891
SpaceKatt <spacekattpoispin@gmail.com>
2021-05-02 15:19:39 -0700

    Add list of all output types

--- src/interfaces/output.ts
+++ src/interfaces/output.ts
@@ -2,6 +2,8 @@ export enum OutputType {
   prettyJson = 'prettyJson',
 }
 
+export const allOutputTypes: string[] = Object.keys(OutputType);
+
 export interface Output {
   // eslint-disable-next-line @typescript-eslint/no-explicit-any
   print(data: any): void;
