fd10dd913a3ed7deeb56ca4a4fcaf2ac0417accc
SpaceKatt <spacekattpoispin@gmail.com>
2021-05-02 15:27:51 -0700

    update debug msg

--- src/factories/outputFactory.ts
+++ src/factories/outputFactory.ts
@@ -7,7 +7,7 @@ export class OutputFactory {
       case OutputType.prettyJson:
         return new JsonOutput();
       default:
-        throw new TypeError(`Unknown type ${kind} given to OutputFactory`);
+        throw new TypeError(`Unknown type "${kind}" given to OutputFactory`);
     }
   }
 }
